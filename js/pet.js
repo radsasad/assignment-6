96502db8e2be4144edf16e97dd8d57e71b77c76c
radsasad <asad.rads@gmail.com>
2024-10-06 12:24:10 +0600

    first commit

---
+++ js/pet.js
@@ -0,0 +1,66 @@
+const loadCategory = () => {
+    fetch('https://openapi.programming-hero.com/api/peddy/categories')
+        .then(res => res.json())
+        .then(data => displayCategory(data.categories))
+        .catch(error => console.log(error))
+}
+
+const loadAllPets = () => {
+    fetch('https://openapi.programming-hero.com/api/peddy/pets')
+        .then(res => res.json())
+        .then(data => displayAllPets(data.pets))
+        .catch(error => console.log(error))
+}
+
+const loadCategoryPets = (category) => {
+    
+    fetch(`https://openapi.programming-hero.com/api/peddy/category/${category}`)
+    .then(res => res.json())
+    .then(data => displayAllPets(data.data))
+    .catch(error => console.log(error))
+}
+
+const displayAllPets = (pets) => {
+    const petsContainer = document.getElementById('all-pets-container');
+    petsContainer.innerHTML ="";
+    pets.forEach((pet) => {
+        const { petId, breed, category ,image ,pet_name ,date_of_birth ,gender , price} = pet;     
+        const card = document.createElement('div')
+        card.classList = "card bg-zinc-50";
+        card.innerHTML = `
+            <figure class="p-3">
+                            <img src=${image}
+                                alt="pet" class="object-cover rounded-lg h-60" />
+                        </figure>
+                        <div class="px-5 pb-4">
+                            <h2 class="font-bold card-title">${pet_name}</h2>
+                            <p class="py-1 text-sm font-semibold text-zinc-500"><i class="fa-solid fa-border-all"></i> Breed: ${breed == null  ? " data not found" :  breed}</p>
+                            <p class="py-1 text-sm font-semibold text-zinc-500"><i class="fa-solid fa-calendar-days"></i> Birth: ${date_of_birth == null ? "data not found"  : date_of_birth }</p>
+                            <p class="py-1 text-sm font-semibold text-zinc-500"><i class="fa-solid fa-mercury"></i> Gender: ${ gender == null ? "data not found"  : gender}</p>
+                            <p class="py-1 text-sm font-semibold text-zinc-500"><i class="fa-solid fa-dollar-sign"></i> Price : ${ price == null ? "Price is not given"  : `${price} $` } </p>
+                            <div class="flex justify-between mt-3 card-actions">
+                                <button class="px-4 py-1 text-base font-semibold rounded-md text-cyan-900 bg-slate-200"><i class="fa-regular fa-thumbs-up"></i></button>
+                                <button class="px-4 py-1 text-base font-semibold rounded-md text-cyan-900 bg-slate-200">Adopt</button>
+                                <button class="px-4 py-1 text-base font-semibold rounded-md text-cyan-900 bg-slate-200">Details</button>
+                            </div>
+                        </div>
+        
+        `
+        petsContainer.append(card)
+    })
+}
+
+const displayCategory = (categories) => {
+    categories.forEach((item) => {
+        const { category, category_icon , id} = item;
+        const categoriesContainer = document.getElementById('category-container');
+        const div = document.createElement('div');
+        div.innerHTML = `
+        <button onclick="loadCategoryPets('${category}')" class="flex items-center text-xl font-semibold btn" type="button"><img class="w-10 h-10"
+                        src=${category_icon} alt="" srcset=""> ${category}</button>
+        `
+        categoriesContainer.append(div)
+    })
+}
+loadAllPets()
+loadCategory()
